eb6b7bd0c5ae37b329393e10425f7995c34139cd
OmkarK <okollure@gmail.com>
2026-02-09 00:14:13 +0530

    Area of square

---
+++ Cpp_DSA/Area_square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float side;
+    cout<<"Enter the side of square: ";
+    cin>>side;
+    float area = side * side;
+    cout<<"Area of square = "<<area<<endl;
+    return 0;
+}
