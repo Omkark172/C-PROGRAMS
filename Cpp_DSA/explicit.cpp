7b6b953a6e04505b0ea1a6f24947ddb8695422e1
OmkarK <okollure@gmail.com>
2026-02-10 00:06:18 +0530

    Explicit TypeCasting done by programmer

---
+++ Cpp_DSA/explicit.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float PI = 3.14; 
+    cout<<(int)(PI)<<"\n"; 
+    cout<<((float)10/3)<<"\n";  
+    cout<<(char)('A' + 1)<<"\n"; 
+
+    return 0;
+}
