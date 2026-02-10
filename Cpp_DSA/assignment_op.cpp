012cb2d7b22108fbda00075ec56b6f0f2e763b3b
OmkarK <okollure@gmail.com>
2026-02-10 23:47:34 +0530

    Assignment Operator in Cpp

---
+++ Cpp_DSA/assignment_op.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =10;
+    a += 5;
+    cout<<" a = "<<a<<endl;//15
+    a -= 5;
+    cout<<" a = "<<a<<endl;//10
+    a *= 5;
+    cout<<" a = "<<a<<endl;//50
+    a /= 5;
+    cout<<" a = "<<a<<endl; //10
+
+    return 0;
+}
