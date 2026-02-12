b9c6f4b3fe01c79639267ded7d18304ca1470099
OmkarK <okollure@gmail.com>
2026-02-12 13:47:48 +0530

    Conditional Statements

---
+++ Cpp_DSA/Conditional_Statements/Calculator1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int a,b;
+    char op;
+    cout<<"Enter first number: ";
+    cin>>a;
+    cout<<"Enter second number: ";
+    cin>>b;
+    cout<<"Enter an operator (+, -, *, /): ";
+    cin>>op;
+
+    if(op == '+'){
+        cout<<"Result: "<<a+b<<endl;
+    } else if(op == '-'){
+        cout<<"Result: "<<a-b<<endl;
+    } else if(op == '*'){
+        cout<<"Result: "<<a*b<<endl;
+    } else if(op == '/'){
+        if(b != 0){
+            cout<<"Result: "<<a/b<<endl;
+        } else {
+            cout<<"Error: Division by zero!"<<endl;
+        }
+    } else {
+        cout<<"Invalid operator!"<<endl;
+    }
+
+    return 0;
+}
