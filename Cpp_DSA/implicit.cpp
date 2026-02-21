ed04424cb08bbd0bef6adf182b1fb62e5992ff8a
Omkark172 <omkarkollure9@gmail.com>
2026-02-21 13:41:26 +0530

    Implicit conversion in Cpp done by compiler itself to avoid data loss

--- Cpp_DSA/implicit.cpp
+++ Cpp_DSA/implicit.cpp
@@ -4,5 +4,6 @@ int main(){
     cout<<(10/3)<<"\n"; // implicit conversion
     cout<<(10.0/3)<<"\n"; // implicit conversion
     cout<<(10/3.0)<<"\n"; // implicit conversion
+    cout<<('A' + 1)<<"\n"; // implicit conversion
     return 0;
 }
