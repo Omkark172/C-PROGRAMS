cd32d2abf5a9b6a9bc0f9077a597536a8c7d5892
Omkark172 <omkarkollure9@gmail.com>
2026-02-21 13:41:25 +0530

    Taking input from user in Cpp

---
+++ Cpp_DSA/input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+    cout<<"Enter a :";
+    cin>>a;
+    cout<<"Enter b :";
+    cin>>b;
+    int sum = a + b;
+    int product = a * b;
+    int subtraction = a - b;
+    cout<<"Sum = "<<sum<<endl;
+    cout<<"Product = "<<product<<endl;
+    cout<<"Subtraction = "<<subtraction<<endl;
+    return 0;
+}
