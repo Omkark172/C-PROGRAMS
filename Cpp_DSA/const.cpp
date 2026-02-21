59153e0baf257798337eba20bb110425672a1cda
Omkark172 <omkarkollure9@gmail.com>
2026-02-21 13:41:26 +0530

    Const in cpp

---
+++ Cpp_DSA/const.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const float PI = 3.14; // constant variable
+    const float g = 9.8; // constant variable
+    cout<<PI<<"\n";
+    cout<<g<<"\n";
+    return 0;
+} 
