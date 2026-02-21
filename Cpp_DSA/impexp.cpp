11bd95be128800d6017a6b99ea81f97fc9431693
Omkark172 <omkarkollure9@gmail.com>
2026-02-21 13:41:26 +0530

    Questions on impilicit and explicit typecasting

---
+++ Cpp_DSA/impexp.cpp
@@ -0,0 +1,11 @@
+// Predict output of cout<<(bool)3+2;
+//Predict output of cout<<(23.5+2+'A');
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<(bool)3+2<<"\n";
+    cout<<(23.5+2+'A')<<"\n";
+
+    return 0;
+}
