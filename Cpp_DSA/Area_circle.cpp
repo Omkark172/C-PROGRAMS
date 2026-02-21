0ae80881a7003590f11019b6beec9c3faacd1977
Omkark172 <omkarkollure9@gmail.com>
2026-02-21 13:41:26 +0530

    Area of circle

---
+++ Cpp_DSA/Area_circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#define PI 3.14
+using namespace std;
+int main(){
+    float radius;
+    cout<<"Enter the radius of circle: ";
+    cin>>radius;
+    float area = PI * radius * radius;
+    cout<<"Area of circle = "<<area<<endl;
+    return 0;
+}
