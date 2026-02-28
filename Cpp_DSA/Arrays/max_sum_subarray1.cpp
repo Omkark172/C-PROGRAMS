8a1f5a4492dfcd36724fed72f0928d1f8db8f471
Omkark172 <omkarkollure9@gmail.com>
2026-02-28 14:24:16 +0530

    Max subarray sum slightly optimized approach Time Comp- O(n^2)

---
+++ Cpp_DSA/Arrays/max_sum_subarray1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+// void maxSumSubarray(int *arr , int n){
+//     int maxSum = INT_MIN; 
+//     for(int start = 0 ; start < n ; start++){
+//         for(int end = start ; end < n ; end++){
+//              int currSum = 0;
+//            // cout << "(" << start << "," << end << ")
+//             for(int i = start ; i <= end ; i++){
+//                 currSum += arr[i];
+//             }
+//             cout << currSum << ",";
+//             maxSum = max(maxSum , currSum); // In cpp header max their is a function called max 
+//         }
+//         cout<<endl;
+//     }
+//     cout << "Maximum sum is =" << maxSum << endl;
+// }
+
+void maxSumSubarray2(int *arr , int n){
+    int maxSum = INT_MIN; 
+    for(int start = 0 ; start < n ; start++){ //start = 2
+        int currSum = 0;
+        for(int end = start ; end < n ; end++){ // end = 2,3,4,5
+            currSum += arr[end];
+            maxSum = max(maxSum , currSum); // In cpp header max their is a function called max 
+        }
+    }
+    cout << "Maximum subarray sum is =" << maxSum << endl;
+}
+
+int main(){
+    int arr[6] = {2,-3,6,-5,4,2};
+    int n = sizeof(arr)/sizeof(int);
+    maxSumSubarray2(arr,n);
+    return 0;
+}
