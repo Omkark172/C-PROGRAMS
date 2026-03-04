f09f4b53bec188806dd0da0e6ed33492f920ecd4
Omkark172 <omkarkollure9@gmail.com>
2026-03-05 00:32:56 +0530

    Pass by reference using Pointers

---
+++ Cpp_DSA/Pointers/pass_by_ref_pointers.cpp
@@ -0,0 +1,17 @@
+//Pass by reference using pointers 
+#include<iostream>
+using namespace std;
+
+void changeA(int *ptr){
+    *ptr = 20;
+    cout<<*ptr<<endl;
+}
+
+int main(){
+int a = 10;
+changeA(&a);
+
+cout<<a<<endl;
+
+    return 0;
+}
