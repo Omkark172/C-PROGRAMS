598af4f8f933ecf5cea9650ca5286181066e6066
Omkark172 <omkarkollure9@gmail.com>
2026-03-05 23:57:24 +0530

    Practice Questions

---
+++ Cpp_DSA/Pointers/ptr6.cpp
@@ -0,0 +1,12 @@
+//What is the type of a and b 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float* a , b;
+
+    //a - float pointer
+    //b - normal float variable 
+    return 0;
+}
---
+++ Cpp_DSA/Pointers/ptr7.cpp
@@ -0,0 +1,11 @@
+//Can we initialize a pointer with value 0
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int *ptr = 0; //Same thing as creating a null pointer 
+    cout<<ptr<<endl;
+
+    return 0;
+}
