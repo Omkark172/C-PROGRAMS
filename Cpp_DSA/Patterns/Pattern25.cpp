5a0ff4a31258ac1480177ca6b78277ff58441e66
Omkark172 <omkarkollure9@gmail.com>
2026-03-07 14:39:07 +0530

    Hollow Pattern Incomplete

---
+++ Cpp_DSA/Patterns/Pattern25.cpp
@@ -0,0 +1,32 @@
+//    *
+//   * *
+//  *   *
+// * * * *
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+//     int n =4;
+//    //1st part
+//     for(int i = 1 ; i<=n ; i++){
+//         for(int j = 1 ; j <= n-i ; j++){
+//             cout<<" ";
+//         }
+//         for(int j = 1 ; j <= n-3 ; j++){
+//             cout<< "*";
+//         }
+//         if(i>1){
+//             for(int j=2 ; j< (2*i)-1 ; j++){
+//                 cout<<" ";
+//             }
+//             for(int j = 1 ; j<= n-3 ; j++){
+//                 cout<<"*";
+//             }
+//         }
+//         cout<<endl;
+    //}
+
+    return 0;
+}
