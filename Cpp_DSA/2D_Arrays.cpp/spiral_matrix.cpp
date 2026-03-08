e197392f071ad01048bb38824a1273e9d6bcd96d
Omkark172 <omkarkollure9@gmail.com>
2026-03-09 00:01:28 +0530

    Spiral Matrix

--- Cpp_DSA/2D_Arrays.cpp/spiral_matrix.cpp
+++ Cpp_DSA/2D_Arrays.cpp/spiral_matrix.cpp
@@ -33,10 +33,38 @@ void spiralMatrix(int mat[][4] , int n , int m){
     srow++; scol++ ;
     erow--; ecol--;
 
-}
+}}
+
+void spiralMatrix2(int mat[][4] , int n , int m){
+    int srow = 0 , scol =0;
+    int erow = n-1 , ecol = m-1;
+
+    while(srow <= erow && scol <= ecol){
+    //top
+    for(int j=scol ; j<=ecol ; j++){
+        cout<< mat[srow][j]<<" ";
+    }
+    //right
+    for(int i = srow+1 ; i<=erow ; i++){
+        cout<< mat[i][ecol] <<" ";
+    }
+
+    //bottom
+    for(int j =ecol-1 ; j>= scol ; j--){
+        cout<<mat[erow][j]<<" ";
+    }
+
+    //left
+    for(int i=erow-1 ; i>=srow+1 ; i--){
+        cout<<mat[i][scol]<<" ";
+    }
+    srow++; scol++ ;
+    erow--; ecol--;
+
+}}
 
 
-}
+
 
 int main(){
     int matrix[4][4] = {{1,2,3,4},
@@ -45,6 +73,12 @@ int main(){
                         {13,14,15,16}};
     
     spiralMatrix(matrix , 4 , 4);
+
+    int matrix2[3][4] = {{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12}};
+
+    spiralMatrix2(matrix2 , 3 , 4);
 
     return 0;
 }
