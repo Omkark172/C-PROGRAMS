dcf1ebb83ca103aa2813bc4a7c4d263b545b5dab
Omkark172 <omkarkollure9@gmail.com>
2026-03-09 16:18:34 +0530

    Spiral Matrix

--- Cpp_DSA/2D_Arrays.cpp/spiral_matrix.cpp
+++ Cpp_DSA/2D_Arrays.cpp/spiral_matrix.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void spiralMatrix(int mat[][4] , int n , int m){
+void spiralMatrix(int mat[][4] , int n , int m){ //odd matrix 
     int srow = 0 , scol =0;
     int erow = n-1 , ecol = m-1;
 
@@ -33,37 +33,9 @@ void spiralMatrix(int mat[][4] , int n , int m){
     srow++; scol++ ;
     erow--; ecol--;
 
-}}
-
-void spiralMatrix2(int mat[][4] , int n , int m){
-    int srow = 0 , scol =0;
-    int erow = n-1 , ecol = m-1;
-
-    while(srow <= erow && scol <= ecol){
-    //top
-    for(int j=scol ; j<=ecol ; j++){
-        cout<< mat[srow][j]<<" ";
-    }
-    //right
-    for(int i = srow+1 ; i<=erow ; i++){
-        cout<< mat[i][ecol] <<" ";
-    }
-
-    //bottom
-    for(int j =ecol-1 ; j>= scol ; j--){
-        cout<<mat[erow][j]<<" ";
-    }
-
-    //left
-    for(int i=erow-1 ; i>=srow+1 ; i--){
-        cout<<mat[i][scol]<<" ";
-    }
-    srow++; scol++ ;
-    erow--; ecol--;
-
-}}
-
-
+}
+    cout<<endl;
+}
 
 
 int main(){
@@ -78,7 +50,7 @@ int main(){
                         {5,6,7,8},
                         {9,10,11,12}};
 
-    spiralMatrix2(matrix2 , 3 , 4);
+    spiralMatrix(matrix2 , 3 , 4);
 
     return 0;
 }
