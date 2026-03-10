1a4236179c9857a397d63826c5a8c32dea4ba15f
Omkark172 <omkarkollure9@gmail.com>
2026-03-11 00:29:37 +0530

    StairCase Search Time Complexity - O(n+m)

--- Cpp_DSA/2D_Arrays.cpp/search_in_sorted_matrix1.cpp
+++ Cpp_DSA/2D_Arrays.cpp/search_in_sorted_matrix1.cpp
@@ -23,24 +23,24 @@ int searchinSortedMatrix(int mat[][4] , int n , int m , int key){
     
 }
 
-// int searchinSortedMatrix2(int mat[][4] , int n , int m , int key){
-//     int i=n-1 , j = 0;
-//     while(i < n && j>=0){
-//         if(mat[i][j] == key){
-//             cout<<"found at cell ("<<i<<","<<j<<")\n";
-//             return true;
-//         }else if(mat[i][j] < key){
-//             //right
-//             j++;
-//         }else{
-//             //top
-//             i--;
-//         }
-//     }
-//     cout<<" key not found\n";
-//     return -1;
+int searchinSortedMatrix2(int mat[][4] , int n , int m , int key){
+    int i=n-1 , j = 0;
+    while(i >= 0 && j < m){
+        if(mat[i][j] == key){
+            cout<<"found at cell ("<<i<<","<<j<<")\n";
+            return true;
+        }else if(mat[i][j] < key){
+            //right
+            j++;
+        }else{
+            //top
+            i--;
+        }
+    }
+    cout<<" key not found\n";
+    return -1;
     
-// }
+}
 
 int main(){
     int matrix[4][4] = {{10,20,30,40},
