33ef5cea182a1cc83c42c95b9f910bd024311f99
Omkark172 <omkarkollure9@gmail.com>
2026-03-12 23:38:14 +0530

    Bubble Sort Time Complexity - O(N^2)

---
+++ Cpp_DSA/Sorting/bubble_sort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[] , int n){
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void bubbleSort(int arr[] , int n){
+    for(int i = 0 ; i < n-1 ; i++){
+         bool isSwap = false;
+        for(int j = 0 ; j <n-i-1 ; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j] , arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if(isSwap == false){
+            //Array is already sorted 
+            return;
+        }
+    }
+}
+
+
+int main(){
+    int arr[5] = {5,2,3,1,4};
+
+    bubbleSort(arr , 5);
+    printArray(arr , 5);
+
+    return 0;
+}
