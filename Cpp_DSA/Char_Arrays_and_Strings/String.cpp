64569761d4ee0f44b39961a993f1ae55e2b6e591
Omkark172 <omkarkollure9@gmail.com>
2026-03-27 15:01:12 +0530

    String Input from user

---
+++ Cpp_DSA/Char_Arrays_and_Strings/String.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin , str);
+    cout<<str<<endl;
+    cout<<str[0]<<endl;
+    cout<<str[1]<<endl;
+    cout<<str[2]<<endl;
+    cout<<str[3]<<endl;
+    cout<<str[4]<<endl;
+
+
+    return 0;
+}
